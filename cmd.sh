60b9d368231d512f9bd5a4d4b62dc2c26bcd1ab4
plasmadrive <gareth.badenhorst@gmail.com>
2016-10-30 20:22:37 +0000

    Added option for unit tests

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then 
     echo "Running Development Server"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+    echo "Running unit tests"
+    exec python "test.py"
 else
     echo "Running Production Server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
